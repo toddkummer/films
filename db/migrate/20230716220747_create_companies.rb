1a8d10d7079fe331b1043668271628be144ae26a
Todd Kummer <todd@rockridgesolutions.com>
2023-07-16 15:13:14 -0700

    Add scaffold for Company

---
+++ db/migrate/20230716220747_create_companies.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateCompanies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :companies do |t|
+      t.string :name, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_16_214617) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_16_220747) do
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "locations", force: :cascade do |t|
     t.string "name", null: false
     t.integer "find_neighborhood_id"
