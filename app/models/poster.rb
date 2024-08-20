9faddeb6bc812950b17a099dee31d333f3a71160
Todd Kummer <todd@rockridgesolutions.com>
2024-08-20 16:12:11 -0700

    Refactor Poster class methods
    - make lookup_and_create private
    - memoize the poster client

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -4,17 +4,24 @@
 class Poster < ApplicationRecord
   belongs_to :film
 
-  def self.for_film(film_id)
-    Poster.find_by(film_id: film_id) ||
-      lookup_and_create(film_id)
-  end
+  class << self
+    def for_film(film_id)
+      Poster.find_by(film_id: film_id) ||
+        lookup_and_create(film_id)
+    end
 
-  def self.lookup_and_create(film_id)
-    film = Film.find(film_id)
-    filename = TMDB::PosterClient.new(Rails.application.config.settings.tmdb_api_read_access_token,
-                                      Rails.logger)
-                                 .for_movie(film.name, film.release_year)
-    film.create_poster!(filename: filename)
+    private
+
+    def lookup_and_create(film_id)
+      film = Film.find(film_id)
+      filename = poster_client.for_movie(film.name, film.release_year)
+      film.create_poster!(filename: filename)
+    end
+
+    def poster_client
+      @poster_client ||= TMDB::PosterClient.new(Rails.application.config.settings.tmdb_api_read_access_token,
+                                                Rails.logger)
+    end
   end
 
   def url
