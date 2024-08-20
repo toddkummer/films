32955b6dab710d482fdce93d394812bed7cac8f9
Todd Kummer <todd@rockridgesolutions.com>
2024-08-20 16:52:21 -0700

    Add tests for Poster.for_film

--- test/models/poster_test.rb
+++ test/models/poster_test.rb
@@ -4,8 +4,28 @@ require 'test_helper'
 
 module Films
   class PosterTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    def test_for_film_with_poster
+      film = films(:one)
+      assert_equal film.poster, Poster.for_film(film.id)
+    end
+
+    def test_for_film_without_poster_calls_poster_client_for_movie
+      film = films(:no_poster)
+      pcm = poster_client_mock(film.name, film.release_year)
+      Poster.stub :poster_client, pcm do
+        poster = Poster.for_film(film.id)
+        assert_equal '/the_poster', poster.filename
+      end
+
+      assert_mock pcm
+    end
+
+    private
+
+    def poster_client_mock(name, release_year)
+      mock = Minitest::Mock.new
+      mock.expect :for_movie, '/the_poster', [name, release_year]
+      mock
+    end
   end
 end
