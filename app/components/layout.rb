5f2de9b0c47a8cd2e9dc2a2a6c6edef42715f3d9
Todd Kummer <todd@rockridgesolutions.com>
2025-12-16 19:48:27 -0800

    Update integrity sha on Font Awesome stylesheet

--- app/components/layout.rb
+++ app/components/layout.rb
@@ -41,7 +41,7 @@ module Components
            href: 'https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css'
       link rel: 'stylesheet',
            href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css',
-           integrity: 'sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==',
+           integrity: 'sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==',
            crossorigin: 'anonymous',
            referrerpolicy: 'no-referrer'
       link rel: 'stylesheet',
